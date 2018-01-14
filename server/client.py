387635b423949b3d66899877c152a6eea7966895
Eleftherios P. Loukas <eleftheriosloukas@gmail.com>
2018-01-14 17:34:58 +0200

    Minor refinements

--- server/client.py
+++ server/client.py
@@ -4,14 +4,19 @@ import asyncio
 import websockets
 
 # youtube link that causes exception
-youtube_link = 'https://www.youtube.com/watch?v=Eq3CuMDXaP'
+#youtube_link = 'https://www.youtube.com/watch?v=Eq3CuMDXaP'
 
 # youtube link that is OK
-#youtube_link = 'https://www.youtube.com/watch?v=BaYATSpDtHA'
+youtube_link = 'https://www.youtube.com/watch?v=BaYATSpDtHA'
 
-# Sends a youtube link to server
-# And waits validation
-async def send_link():
+
+'''
+    send_youtube_link
+
+    -1- Sends YouTube URL to server
+    -2- Waits for validation response (OK/NOT OK)
+'''
+async def send_youtube_link():
     async with websockets.connect('ws://localhost:8765') as websocket:
         print("Client started!")
         await websocket.send(youtube_link)
@@ -19,4 +24,4 @@ async def send_link():
         validation = await websocket.recv()
         print("< {}".format(validation))
 
-asyncio.get_event_loop().run_until_complete(send_link())
+asyncio.get_event_loop().run_until_complete(send_youtube_link())
--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 import youtube_dl
 
- # options for youtube_dl
+ # Options for youtube_dl
 ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
@@ -13,23 +13,31 @@ ydl_opts = {
     'outtmpl': 'downloadedsongs/%(title)s.%(ext)s'
 }
 
+'''
+    check_youtube_link(websocket, path)
 
-async def check_link(websocket, path):
-    video = await websocket.recv()
-    print("< {}".format(video))
+    -1- Gets a videoURL from the websocket
+    -2- Checks if valid
+    -3- Sends appropriate response ('OK/'NOT OK')
+'''
+async def check_youtube_link(websocket, path):
+    videoURL = await websocket.recv()
+    print("< Youtube link given: {}".format(videoURL))
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
-            info_dict = ydl.extract_info(video, download=False)
+            info_dict = ydl.extract_info(videoURL, download=False)
             video_url = info_dict.get("url", None)
             video_id = info_dict.get("id", None)
             video_title = info_dict.get('title', None)
 
+            print("Sending OK to client..")
             await websocket.send('OK')
         except Exception as exception: # Catch all exceptions
-            print(exception)
+            print(exception) # gets printed in any case
+            print("Sending NOT OK to client..")
             await websocket.send('NOT OK')
 
-start_server = websockets.serve(check_link, 'localhost', 8765)
+start_server = websockets.serve(check_youtube_link, 'localhost', 8765)
 print("Server started!")
 
 asyncio.get_event_loop().run_until_complete(start_server)
