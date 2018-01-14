8fd6c44e2b2cdb9297316cf8c08fba0810504132
Eleftherios P. Loukas <eleftheriosloukas@gmail.com>
2018-01-14 17:23:06 +0200

    Handle exceptions when youtube link is not working

--- server/client.py
+++ server/client.py
@@ -3,10 +3,17 @@
 import asyncio
 import websockets
 
+# youtube link that causes exception
 youtube_link = 'https://www.youtube.com/watch?v=Eq3CuMDXaP'
 
+# youtube link that is OK
+#youtube_link = 'https://www.youtube.com/watch?v=BaYATSpDtHA'
+
+# Sends a youtube link to server
+# And waits validation
 async def send_link():
     async with websockets.connect('ws://localhost:8765') as websocket:
+        print("Client started!")
         await websocket.send(youtube_link)
 
         validation = await websocket.recv()
--- server/server.py
+++ server/server.py
@@ -2,7 +2,8 @@ import asyncio
 import websockets
 import youtube_dl
 
-ydl_opts = { # options for youtube_dl
+ # options for youtube_dl
+ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
@@ -12,18 +13,24 @@ ydl_opts = { # options for youtube_dl
     'outtmpl': 'downloadedsongs/%(title)s.%(ext)s'
 }
 
+
 async def check_link(websocket, path):
     video = await websocket.recv()
     print("< {}".format(video))
-    with youtube_dl.YoutubeDL(youtube_dl_opts) as ydl:
-        info_dict = ydl.extract_info(video, download=False)
-        video_url = info_dict.get("url", None)
-        video_id = info_dict.get("id", None)
-        video_title = info_dict.get('title', None)
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info_dict = ydl.extract_info(video, download=False)
+            video_url = info_dict.get("url", None)
+            video_id = info_dict.get("id", None)
+            video_title = info_dict.get('title', None)
 
-    await websocket.send('It looks ok')
+            await websocket.send('OK')
+        except Exception as exception: # Catch all exceptions
+            print(exception)
+            await websocket.send('NOT OK')
 
 start_server = websockets.serve(check_link, 'localhost', 8765)
+print("Server started!")
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
